f7ab8de3d9d180927db2b5713ae820c050f97668
居正中 <AlanJui.1960@gmail.com>
2023-04-13 13:47:19 +0800

    調整需要使用之 Plugins

--- lua/plugins-rc/init.lua
+++ lua/plugins-rc/init.lua
@@ -3,62 +3,62 @@
 -- 載入各擴充套件(plugins) 的設定
 -----------------------------------------------------------
 -- Lua initialization file
-vim.cmd [[colorscheme nightfly]]
+vim.cmd([[colorscheme nightfly]])
 -- Load Which-key
 -- 提供【選單】式的指令操作
-require("config.plugins-rc.which-key")
+require("plugins-rc.which-key")
 -- Neovim kernel
-require("config.plugins-rc.nvim-treesitter")
+require("plugins-rc.nvim-treesitter")
 
 -- lsp
 -- require("lsp")
 
 -- status line
--- require("config.plugins-rc.lualine-material")
-require("config.plugins-rc.tabline")
+-- require("plugins-rc.lualine-material")
+-- require("plugins-rc.tabline")
 
 -- User Interface
-require("config.plugins-rc.nvim-web-devicons")
-require("config.plugins-rc.indent-blankline")
--- require("config.plugins-rc.nvim-lightbulb")
+-- require("plugins-rc.nvim-web-devicons")
+-- require("plugins-rc.indent-blankline")
+-- require("plugins-rc.nvim-lightbulb")
 
 -- files management
-require("config.plugins-rc.telescope-nvim")
+-- require("plugins-rc.telescope-nvim")
+-- require("plugins-rc.nvim-tree")
 -- require("my-telescope")
-require("config.plugins-rc.nvim-tree")
--- require("config.plugins-rc.harpoon")
+-- require("plugins-rc.harpoon")
 
 -- editting tools
-require("config.plugins-rc.comment-nvim")
-require("config.plugins-rc.nvim-ufo-rc")
-require("config.plugins-rc.undotree")
-require("config.plugins-rc.trim-nvim")
-require("config.plugins-rc.autopairs")
-require("config.plugins-rc.nvim-ts-autotag")
-vim.cmd([[runtime lua.config.plugins-rc.vim-surround.rc.vim]])
-vim.cmd([[runtime lua.config.plugins-rc.tagalong-vim.rc.vim]])
+-- require("plugins-rc.comment-nvim")
+require("plugins-rc.nvim-ufo-rc")
+require("plugins-rc.undotree")
+require("plugins-rc.trim-nvim")
+require("plugins-rc.autopairs")
+require("plugins-rc.nvim-ts-autotag")
+vim.cmd([[runtime lua.plugins-rc.vim-surround.rc.vim]])
+vim.cmd([[runtime lua.plugins-rc.tagalong-vim.rc.vim]])
 
 -- programming
-require("config.plugins-rc.toggleterm")
-require("config.plugins-rc.consolation-nvim")
-require("config.plugins-rc.yabs")
+-- require("plugins-rc.toggleterm")
+-- require("plugins-rc.consolation-nvim")
+require("plugins-rc.yabs")
 
 -- versional control
-require("config.plugins-rc.gitsigns")
-require("config.plugins-rc.neogit")
-require("config.plugins-rc.vim-gist")
--- vim.cmd([[ runtime lua.config.plugins-rc.vim-signify.rc.vim]])
+require("plugins-rc.gitsigns")
+require("plugins-rc.neogit")
+require("plugins-rc.vim-gist")
+-- vim.cmd([[ runtime lua.plugins-rc.vim-signify.rc.vim]])
 
 -- Utilities
-vim.cmd([[runtime lua.config.plugins-rc.vim-instant-markdown.rc.vim]])
-vim.cmd([[runtime lua.config.plugins-rc.markdown-preview-nvim.rc.vim]])
-vim.cmd([[runtime lua.config.plugins-rc.plantuml-previewer.rc.vim]])
-vim.cmd([[runtime lua.config.plugins-rc.vimtex.rc.vim]])
-vim.cmd([[runtime lua.config.plugins-rc.bracey.rc.vim]])
+vim.cmd([[runtime lua.plugins-rc.vim-instant-markdown.rc.vim]])
+vim.cmd([[runtime lua.plugins-rc.markdown-preview-nvim.rc.vim]])
+vim.cmd([[runtime lua.plugins-rc.plantuml-previewer.rc.vim]])
+vim.cmd([[runtime lua.plugins-rc.vimtex.rc.vim]])
+vim.cmd([[runtime lua.plugins-rc.bracey.rc.vim]])
 
 -- debug & unit testing
 -- require("debugger")
 -- require("unit-test")
 
 -- AI tools
--- require("config.plugins-rc.ChatGPT-rc")
+-- require("plugins-rc.ChatGPT-rc")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,214 +6,49 @@ return {
   "nvim-lua/plenary.nvim",
   -- Tools to migrating init.vim to init.lua
   "norcalli/nvim_utils",
-  -----------------------------------------------------------
-  -- Completion: for auto-completion/suggestion/snippets
-  -----------------------------------------------------------
-  -- A completion plugin for neovim coded in Lua.
   {
-    -- Completion framework
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      -- LSP completion source
-      "hrsh7th/cmp-nvim-lsp",
-      -- Useful completion sources
-      "hrsh7th/cmp-nvim-lua", -- nvim-cmp source for buffer words
-      "hrsh7th/cmp-buffer", -- nvim-cmp source for filesystem paths
-      "hrsh7th/cmp-path", -- nvim-cmp source for math calculation
-      "hrsh7th/cmp-calc",
-      "hrsh7th/cmp-emoji",
-      "hrsh7th/cmp-cmdline",
-      -- LuaSnip completion source for nvim-cmp
-      "saadparwaiz1/cmp_luasnip",
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    opts = {
+      -- background_colour = "#A3CCBE",
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
     },
-  },
-  -- Snippet Engine for Neovim written in Lua.
-  {
-    "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    version = "<CurrentMajor>.*",
-    -- install jsregexp (optional!).
-    build = "make install_jsregexp",
-  },
-  -- Snippets collection for a set of different programming languages for faster development
-  "rafamadriz/friendly-snippets",
-  -----------------------------------------------------------
-  -- LSP/LspInstaller: configurations for the Nvim LSP client
-  -----------------------------------------------------------
-  {
-    -- companion plugin for nvim-lspconfig that allows you to seamlessly
-    -- install LSP servers locally
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-  },
-  {
-    -- A collection of common configurations for Neovim's built-in language
-    -- server client
-    "neovim/nvim-lspconfig",
-    "williamboman/mason-lspconfig.nvim",
-    -- helps users keep up-to-date with their tools and to make certain
-    -- they have a consistent environment.
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  -- LSP plugin based on Neovim build-in LSP with highly a performant UI
-  {
-    "glepnir/lspsaga.nvim",
-    event = "LspAttach",
-    dependencies = {
-      { "nvim-tree/nvim-web-devicons" },
-      --Please make sure you install markdown and markdown_inline parser
-      { "nvim-treesitter/nvim-treesitter" },
-    },
-  },
-  --
-  -- All in one LSP plugin (include auto-complete)
-  --
-  -- {
-  --     "VonHeikemen/lsp-zero.nvim",
-  --     branch = "v1.x",
-  --     dependencies = {
-  --         -- LSP Support
-  --         { "neovim/nvim-lspconfig" }, -- Required
-  --         { "williamboman/mason.nvim" }, -- Optional
-  --         { "williamboman/mason-lspconfig.nvim" }, -- Optional
-  --
-  --         -- Autocompletion
-  --         { "hrsh7th/nvim-cmp" }, -- Required
-  --         { "hrsh7th/cmp-nvim-lsp" }, -- Required
-  --         { "hrsh7th/cmp-buffer" }, -- Optional
-  --         { "hrsh7th/cmp-path" }, -- Optional
-  --         { "saadparwaiz1/cmp_luasnip" }, -- Optional
-  --         { "hrsh7th/cmp-nvim-lua" }, -- Optional
-  --
-  --         -- Snippets
-  --         { "L3MON4D3/LuaSnip" }, -- Required
-  --         { "rafamadriz/friendly-snippets" }, -- Optional
-  --
-  --         -- Optional
-  --         { "simrat39/rust-tools.nvim" },
-  --     },
-  -- },
-  -- bridges gap b/w mason & null-ls
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "jose-elias-alvarez/null-ls.nvim",
-    },
-  },
-  -- formatting & linting
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- stylua-nvim is a mini Lua code formatter
-      "ckipp01/stylua-nvim",
-    },
-  },
-  -- vscode-like pictograms for neovim lsp completion items Topics
-  "onsails/lspkind-nvim",
-  --
-  -- automatically highlighting other uses of the current word under the cursor
-  -- 'RRethy/vim-illuminate',
-  --
-  -- Support LSP CodeAction
-  -- 'kosayoda/nvim-lightbulb',
-  --
-  -- additional functionality for typescript server
-  -- (e.g. rename file & update imports)
-  -- 'jose-elias-alvarez/typescript.nvim',
-
-  -----------------------------------------------------------
-  -- AI Tooles
-  -----------------------------------------------------------
-  -- AI code auto-complete
-  "github/copilot.vim",
-  "hrsh7th/cmp-copilot",
-  -----------------------------------------------------------
-  -- Treesitter: for better syntax
-  -----------------------------------------------------------
-  -- Nvim Treesitter configurations and abstraction layer
-  {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  },
-  -- 'nvim-treesitter/playground',
-  -- Additional textobjects for treesitter
-  -- 'nvim-treesitter/nvim-treesitter-textobjects',
-  -----------------------------------------------------------
-  -- colorscheme for neovim written in lua specially made for roshnvim
-  -----------------------------------------------------------
-  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
-  "bluz71/vim-moonfly-colors",
-  "shaeinst/roshnivim-cs",
-  "mhartington/oceanic-next",
-  "folke/tokyonight.nvim",
-  -----------------------------------------------------------
-  -- User Interface
-  -----------------------------------------------------------
-  -- Quick switch between files
-  "ThePrimeagen/harpoon",
-  -- maximizes and restores current window
-  "szw/vim-maximizer",
-  -- tmux & split window navigation
-  "christoomey/vim-tmux-navigator",
-  -- Add indentation guides even on blank lines
-  "lukas-reineke/indent-blankline.nvim",
-  -- Status Line
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-  },
-  {
-    "kdheepak/tabline.nvim",
-    require = { "hoob3rt/lualine.nvim", "nvim-tree/nvim-web-devicons" },
-  },
-  "arkav/lualine-lsp-progress",
-  -- Utility functions for getting diagnostic status and progress messages
-  -- from LSP servers, for use in the Neovim statusline
-  "nvim-lua/lsp-status.nvim",
-  -- Fuzzy files finder
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-live-grep-raw.nvim",
-    },
-  },
-  -- Telescope Extensions
-  -- 'cljoly/telescope-repo.nvim',
-  "nvim-telescope/telescope-file-browser.nvim",
-  "nvim-telescope/telescope-ui-select.nvim",
-  "dhruvmanila/telescope-bookmarks.nvim",
-  "nvim-telescope/telescope-github.nvim",
-  -- Trying command palette
-  "LinArcX/telescope-command-palette.nvim",
-  {
-    "AckslD/nvim-neoclip.lua",
-    config = function()
-      require("neoclip").setup()
+    config = function(_, opts)
+      require("notify").setup(opts)
+      vim.notify = require("notify")
     end,
   },
-  -- dependency for better sorting performance
+  -- Add indentation guides even on blank lines
   {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    run = "make",
-  },
-  "jvgrootveld/telescope-zoxide",
-  -- File explorer: vifm
-  "vifm/vifm.vim",
-  -- vs-code like icons
-  "nvim-tree/nvim-web-devicons",
-  -- File/Flolders explorer:nvim-tree
-  {
-    "kyazdani42/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons", -- optional, for file icon
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      -- char = "▏",
+      char = "│",
+      filetype_exclude = { "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy" },
+      show_trailing_blankline_indent = false,
+      show_current_context = false,
     },
   },
-  -- Screen Navigation
-  { "folke/which-key.nvim" },
+  -- Toggle comments in Neovim
+  {
+    "numToStr/Comment.nvim",
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+    keys = { { "gc", mode = { "n", "v" } }, { "gcc", mode = { "n", "v" } }, { "gbc", mode = { "n", "v" } } },
+    config = function(_, _)
+      local opts = {
+        ignore = "^$",
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+      require("Comment").setup(opts)
+    end,
+  },
   -----------------------------------------------------------
   -- Git Tools
   -----------------------------------------------------------
@@ -238,11 +73,11 @@ return {
   -----------------------------------------------------------
   -- Editting Tools
   -----------------------------------------------------------
-  -- To make Neovim's fold look modern and keep high performance.
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-  },
+  -- -- To make Neovim's fold look modern and keep high performance.
+  -- {
+  --   "kevinhwang91/nvim-ufo",
+  --   dependencies = "kevinhwang91/promise-async",
+  -- },
   -- replace with register contents using motion (gr + motion)
   -- 'inkarkat/vim-ReplaceWithRegister',
   -- surroundings: parentheses, brackets, quotes, XML tags, and more
@@ -250,12 +85,9 @@ return {
     "tpope/vim-surround",
     dependencies = { "tpope/vim-repeat" },
   },
-  -- Toggle comments in Neovim
-  -- 'tpope/vim-commentary',
-  "numToStr/Comment.nvim",
   -- A Neovim plugin for setting the commentstring option based on the cursor
   -- location in the file. The location is checked via treesitter queries.
-  "JoosepAlviste/nvim-ts-context-commentstring",
+  -- "JoosepAlviste/nvim-ts-context-commentstring",
   -- Causes all trailing whitespace characters to be highlighted
   "cappyzawa/trim.nvim",
   -- Multiple cursor editting
@@ -271,91 +103,16 @@ return {
   -----------------------------------------------------------
   -- Coding Tools
   -----------------------------------------------------------
-  -- A pretty list for showing diagnostics, references, telescope results, quickfix and
-  -- location lists to help you solve all the trouble your code is causing.
-  {
-    "folke/trouble.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("trouble").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      })
-    end,
-  },
   -- Yet Another Build System
   {
     "pianocomposer321/yabs.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
-  -- terminal
-  "pianocomposer321/consolation.nvim",
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = true,
-  },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim",
-    },
-  },
-  "nvim-neotest/neotest-plenary",
-  "nvim-neotest/neotest-python",
-  -----------------------------------------------------------
-  -- DAP
-  -----------------------------------------------------------
-  {
-    "mfussenegger/nvim-dap",
-    -- bridges mason.nvim with the nvim-dap plugin - making it
-    -- easier to use both plugins together.
-    "jay-babu/mason-nvim-dap.nvim",
-  },
-  --
-  -- Language specific exensions
-  --
-  -- DAP for Python
-  "mfussenegger/nvim-dap-python",
-  -- DAP for Lua work in Neovim
-  "jbyuki/one-small-step-for-vimkind",
-  -- DAP for Node.js (nvim-dap adapter for vscode-js-debug)
-  -- 'mxsdev/nvim-dap-vscode-js", dependencies = { "mfussenegger/nvim-dap" } },
-  -- {
-  -- 	"microsoft/vscode-js-debug",
-  -- 	opt = true,
-  -- 	run = "npm install --legacy-peer-deps && npm run compile",
-  -- },
-  --
-  -- DAP UI Extensions
-  --
-  -- UI for nvim-dap
-  -- Install icons for dap-ui: https://github.com/ChristianChiarulli/neovim-codicons
-  "folke/neodev.nvim",
-  -- 'rcarriga/nvim-dap-ui',
-  -- Reset nvim-dap-ui to a specific commit
-  {
-    "rcarriga/nvim-dap-ui",
-    tag = "v3.6.4",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
-  },
-
-  -- Inlines the values for variables as virtual text using treesitter.
-  "theHamsta/nvim-dap-virtual-text",
-  -- -- Integration for nvim-dap with telescope.nvim
-  "nvim-telescope/telescope-dap.nvim",
-  -- UI integration for nvim-dat with fzf
-  "ibhagwan/fzf-lua",
-  -- nvim-cmp source for using DAP completions inside the REPL.
-  "rcarriga/cmp-dap",
   -----------------------------------------------------------
   -- Utility
   -----------------------------------------------------------
+  -- File explorer: vifm
+  "vifm/vifm.vim",
   -- Floater Terminal
   "voldikss/vim-floaterm",
   -- Live server
